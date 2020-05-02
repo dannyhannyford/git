de0f21953d9cdb58f30ccb87ce66e5ebc9a61221
dantanna <dannyhannyford@gmail.com>
2020-05-01 18:41:40 -0700

    ratio first format attempt

--- pasteCog/pasteon.py
+++ pasteCog/pasteon.py
@@ -11,9 +11,9 @@ from pytz import timezone
 #from .pastepoints import PastePoints
 from redbot.core.utils.chat_formatting import box, pagify
 
-upemoji_id = 397064398830829569
-downemoji_id = 272737368916754432
-channel_id = 331655111644545027
+upemoji_id = 702371543078010920
+downemoji_id = 702371815418232862
+channel_id = 702989381266309260
 class Pasteon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,6 +21,7 @@ class Pasteon(commands.Cog):
         default_guild = {}
         self.config.register_guild(**default_guild)
         self.config.register_user(karmon=0)
+        self.config.register_user(posts=0)
 
     @commands.command()
     async def ppmonth(self, ctx: commands.Context, top: int = 10):
@@ -40,7 +41,6 @@ class Pasteon(commands.Cog):
         members_sorted = sorted(
             await self._get_all_members(ctx.bot), key=lambda x: x.karmon, reverse=reverse
         )
-
         if len(members_sorted) < top:
             top = len(members_sorted)
         topten = members_sorted[:top]
@@ -49,7 +49,8 @@ class Pasteon(commands.Cog):
         for member in topten:
             highscore += str(place).ljust(len(str(top)) + 1)
             highscore += "{} | ".format(member.name).ljust(18 - len(str(member.karmon)))
-            highscore += str(member.karmon) + "\n"
+            #add ratio here
+            highscore += str(member.karmon) +" ratio: "+ str(round(member.karmon/member.posts,2)) + "\n"
             place += 1
         if highscore != "":
             for page in pagify(highscore, shorten_by=12):
@@ -57,12 +58,12 @@ class Pasteon(commands.Cog):
         else:
             await ctx.send("No one has any karma 🙁")
         '''this clears all users karma counts after leaderboard is posted'''
-        for msg in msglst:
-            for react in msg.reactions:
-                await self._check_reaction(react, -react.count)
+        for member in members_sorted:
+            await self.config.user(member).karmon.set(0)
+            await self.config.user(member).posts.set(0)
 
     async def _get_all_members(self, bot):
-        member_info = namedtuple("Member", "id name karmon")
+        member_info = namedtuple("Member", "id name karmon posts")
         ret = []
         for member in bot.get_all_members():
             if any(member.id == m.id for m in ret):
@@ -70,18 +71,25 @@ class Pasteon(commands.Cog):
             karmon = await self.config.user(member).karmon()
             if karmon ==0:
                 continue
-            ret.append(member_info(id=member.id, name=str(member), karmon=karmon))
+            posts = await self.config.user(member).posts()
+            if posts ==0:
+                continue
+            ret.append(member_info(id=member.id, name=str(member), karmon=karmon, posts=posts))
         return ret
 
     async def _check_reaction(self, reaction: discord.Reaction, count):
         message = reaction.message
         (author, channel, guild) = (message.author, message.channel, message.guild)
+        settings = self.config.user(author)
+        posts = await settings.posts()
+        await settings.posts.set(posts + 1)
         if (isinstance(reaction.emoji, str)):
             return
         if (reaction.emoji.id == upemoji_id):
             await self._add_karmon(author, count)
         if (reaction.emoji.id == downemoji_id):
             await self._add_karmon(author, -count)
+
 
     async def _add_karmon(self, user: discord.User, amount: int):
         settings = self.config.user(user)
