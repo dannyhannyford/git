84f623a611e3d4fe6cab1c9571e7de6235b863aa
dantanna <dannyhannyford@gmail.com>
2020-05-03 19:43:33 -0700

    made sure if msglist is not an object that it wont run

--- pasteCog/pasteon.py
+++ pasteCog/pasteon.py
@@ -28,41 +28,42 @@ class Pasteon(commands.Cog):
         channel = self.bot.get_channel(id = channel_id)
         thirty = self.get_month()
         msglst = await channel.history(limit=1000, after=thirty, oldest_first = False).flatten()
-        for msg in msglst:
-            await self._check_post(msg)
-            for react in msg.reactions:
-                await self._check_reaction(react, react.count)
+        if msglst:
+            for msg in msglst:
+                await self._check_post(msg)
+                for react in msg.reactions:
+                    await self._check_reaction(react, react.count)
 
-        '''LEADERBOARD PART'''
-        reverse = True
-        if top == 0:
-            top = 10
-        elif top < 0:
-            reverse = False
-            top = -top
-        members_sorted = sorted(
-            await self._get_all_members(ctx.bot), key=lambda x: x.karmon, reverse=reverse
-        )
-        if len(members_sorted) < top:
-            top = len(members_sorted)
-        topten = members_sorted[:top]
-        highscore = ""
-        place = 1
-        for member in topten:
-            highscore += str(place).ljust(len(str(top)) + 1)
-            highscore += "{} | ".format(member.name).ljust(18 - len(str(member.karmon)))
-            #add ratio here
-            highscore += str(member.karmon) +" ratio: "+ str(round(member.karmon/member.posts,2)) + "\n"
-            place += 1
-        if highscore != "":
-            for page in pagify(highscore, shorten_by=12):
-                await ctx.send(box(page, lang="py"))
-        else:
-            await ctx.send("No one has any karma 🙁")
-        '''this clears all users karma counts after leaderboard is posted'''
-        for member in members_sorted:
-            await self.config.user(member).karmon.set(0)
-            await self.config.user(member).posts.set(0)
+            '''LEADERBOARD PART'''
+            reverse = True
+            if top == 0:
+                top = 10
+            elif top < 0:
+                reverse = False
+                top = -top
+            members_sorted = sorted(
+                await self._get_all_members(ctx.bot), key=lambda x: x.karmon, reverse=reverse
+            )
+            if len(members_sorted) < top:
+                top = len(members_sorted)
+            topten = members_sorted[:top]
+            highscore = ""
+            place = 1
+            for member in topten:
+                highscore += str(place).ljust(len(str(top)) + 1)
+                highscore += "{} | ".format(member.name).ljust(18 - len(str(member.karmon)))
+                #add ratio here
+                highscore += str(member.karmon) +" ratio: "+ str(round(member.karmon/member.posts,2)) + "\n"
+                place += 1
+            if highscore != "":
+                for page in pagify(highscore, shorten_by=12):
+                    await ctx.send(box(page, lang="py"))
+            else:
+                await ctx.send("No one has any karma 🙁")
+            '''this clears all users karma counts after leaderboard is posted'''
+            for member in members_sorted:
+                await self.config.user(member).karmon.set(0)
+                await self.config.user(member).posts.set(0)
 
     async def _get_all_members(self, bot):
         member_info = namedtuple("Member", "id name karmon posts")
